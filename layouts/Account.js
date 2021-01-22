186b6aea19bd7111762936ae925c1af913fc7f47
crazydev71 <crazy.dev@aol.com>
2021-01-23 04:39:25 +1000

    Update whalestreet js version

--- layouts/Account.js
+++ layouts/Account.js
@@ -3,15 +3,25 @@ import { connect } from 'react-redux'
 import Web3Modal from 'web3modal'
 import WalletConnectProvider from '@walletconnect/web3-provider'
 import Library from 'whalestreet-js'
-import { infuras, isSupportedNetwork, networkLabel, networks } from 'utils/etherscan'
+import { infuras, isSupportedNetwork, networkLabel, networks, networkLabels } from 'utils/etherscan'
 
 const addresses = {
-  Vault: '0x6C66dCC216B2324520bAcB9E3696A9F54238999a',
-  Token0: '0x37DB1941bcc5A687cF8E21aaB7058cDC43DD0b44',
-  Token1: '0xe40E97AE323A1E663E0687FFA88D33c3C44101D3',
-  Market: '0xfF117e9D0598F1808CFAe3Aeef9Cb0A0Bbd1705e',
-  Buyout: '0xb3c48fF6830ff7E9597882b207C7aC8191F7F208',
-  Redeem: '0x8E8804663920fDFE22778eAAF646E12087A5d4C9',
+  1: {
+    Vault: '0x0000000000000000000000000000000000000000',
+    Token0: '0x0000000000000000000000000000000000000000',
+    Token1: '0x0000000000000000000000000000000000000000',
+    Market: '0x0000000000000000000000000000000000000000',
+    Buyout: '0x0000000000000000000000000000000000000000',
+    Redeem: '0x0000000000000000000000000000000000000000',
+  },
+  4: {
+    Vault: '0x6C66dCC216B2324520bAcB9E3696A9F54238999a',
+    Token0: '0x37DB1941bcc5A687cF8E21aaB7058cDC43DD0b44',
+    Token1: '0xe40E97AE323A1E663E0687FFA88D33c3C44101D3',
+    Market: '0xfF117e9D0598F1808CFAe3Aeef9Cb0A0Bbd1705e',
+    Buyout: '0xb3c48fF6830ff7E9597882b207C7aC8191F7F208',
+    Redeem: '0x8E8804663920fDFE22778eAAF646E12087A5d4C9',
+  }
 }
 
 let web3Modal
@@ -20,7 +30,7 @@ const providerOptions = (network) => ({
   walletconnect: {
     package: WalletConnectProvider,
     options: {
-      infuraId: infuras[network],
+      // infuraId: infuras[network],
     },
   },
   // fortmatic: {
@@ -77,8 +87,9 @@ class Account extends Component {
   }
 
   initLibrary(provider) {
+    const contractAddresses = addresses[this.state.session.network]
     if (this.props.library) {
-      this.props.library.setProvider(provider, addresses)
+      this.props.library.setProvider(provider, contractAddresses)
     } else {
       const { dispatch } = this.props
       const handleEvent = (event) => {
@@ -96,7 +107,7 @@ class Account extends Component {
               })
             } else {
               if (event.status !== 0) {
-                this.props.library.setProvider(provider, addresses)
+                this.props.library.setProvider(provider, contractAddresses)
               }
               dispatch({
                 type: 'METAMASK',
@@ -110,7 +121,7 @@ class Account extends Component {
       }
       const library = new Library.B20(provider, {
         onEvent: handleEvent,
-        addresses,
+        addresses: contractAddresses,
       })
       dispatch({
         type: 'INIT_CONTRACTS',
